ca3e5904d64df725215b3b3c7496af3d8dc0da1e
sergey.sekhin <cocVmweA8d>
2020-09-13 20:44:53 +0300

    first commit

---
+++ src/main/java/com/kpi/configclient/ConfigController.java
@@ -0,0 +1,19 @@
+package com.kpi.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class ConfigController {
+
+    @Value("${key: default key}")
+    private String key;
+
+    @GetMapping(value = "/key")
+    public String getKey() {
+        return this.key;
+    }
+}
